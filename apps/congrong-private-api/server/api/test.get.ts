e39d61fdc95bfafd79f9218ee5d9736a3350d0e6
suk.bear <suk.bear.suwu@gmail.com>
2025-04-25 11:00:27 +0800

    feat: 添加 no-cors 模式以绕过 CORS 限制

--- apps/congrong-private-api/server/api/test.get.ts
+++ apps/congrong-private-api/server/api/test.get.ts
@@ -9,7 +9,9 @@ interface TokenResponse {
 }
 
 export default eventHandler(async () => {
-  const tokenApiResponse = await fetch('https://shebei.congrongtech.cn/api/token')
+  const tokenApiResponse = await fetch('https://shebei.congrongtech.cn/api/token', {
+    mode: 'no-cors', // 添加 no-cors 模式以绕过 CORS 限制
+  })
   const tokenResponse = await tokenApiResponse.json() as TokenResponse
   return tokenResponse
 })
