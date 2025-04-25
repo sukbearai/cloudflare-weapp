40cadd4eedb63aaa66c8b6419e12061ed1b1e021
suk.bear <suk.bear.suwu@gmail.com>
2025-04-25 10:49:02 +0800

    feat: 添加 TokenResponse 接口定义并更新事件处理程序以返回 token 响应

--- apps/congrong-private-api/server/api/test.get.ts
+++ apps/congrong-private-api/server/api/test.get.ts
@@ -1,4 +1,15 @@
-// eslint-disable-next-line ts/no-unused-vars
-export default eventHandler(async (event) => {
-  return 'test'
+interface TokenResponse {
+  data: {
+    access_token: string
+    expires_in: number
+    from_cache?: boolean
+  }
+  message: string
+  code: number
+}
+
+export default eventHandler(async () => {
+  const tokenApiResponse = await fetch('https://shebei.congrongtech.cn/api/token')
+  const tokenResponse = await tokenApiResponse.json() as TokenResponse
+  return tokenResponse
 })
