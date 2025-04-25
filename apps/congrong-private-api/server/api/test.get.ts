4266c82c00c8e0b724f87e597ae7d3506193b90f
suk.bear <suk.bear.suwu@gmail.com>
2025-04-25 11:20:03 +0800

    fix: 修复 tokenApiResponse 的处理方式，直接返回响应结果

--- apps/congrong-private-api/server/api/test.get.ts
+++ apps/congrong-private-api/server/api/test.get.ts
@@ -1,17 +1,7 @@
-interface TokenResponse {
-  data: {
-    access_token: string
-    expires_in: number
-    from_cache?: boolean
-  }
-  message: string
-  code: number
-}
-
 export default eventHandler(async () => {
   const tokenApiResponse = await fetch('https://shebei.congrongtech.cn/api/token', {
     mode: 'no-cors', // 添加 no-cors 模式以绕过 CORS 限制
   })
-  const tokenResponse = await tokenApiResponse.json() as TokenResponse
-  return tokenResponse
+  const tokenResponseRes = await tokenApiResponse
+  return tokenResponseRes
 })
