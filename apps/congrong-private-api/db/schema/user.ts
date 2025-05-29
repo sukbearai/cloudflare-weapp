b4ef42a05f4f22bb46b15d26a4fb3876c11f7a74
suk.bear <suk.bear.suwu@gmail.com>
2025-05-29 18:02:25 +0800

    feat: 更新用户表字段

--- apps/congrong-private-api/db/schema/user.ts
+++ apps/congrong-private-api/db/schema/user.ts
@@ -4,4 +4,7 @@ export const usersTable = sqliteTable('users_table', {
   id: int().primaryKey({ autoIncrement: true }),
   nickname: text().notNull(),
   phone: text().notNull(),
+  password: text(),
+  role: text().default('user'),
+  deviceIds: text().default('[]'),
 })
